abc0435623b9540a4a7afa626db1cdb9ba9f3ca2
ccamac01 <chris.camacho327@gmail.com>
2018-08-08 15:43:43 +0530

    Added home button to category pages and headers to main page

--- rango/views.py
+++ rango/views.py
@@ -6,8 +6,9 @@ def index(request):
 	# link = '<a href = "http://127.0.0.1:8000/rango/about/"> about page </a>'
 	# return HttpResponse('Hey there partner ! -Rango' + link)
 	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
 
-	context_dict = {'categories':category_list}
+	context_dict = {'categories':category_list, 'pages':page_list}
 	return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
